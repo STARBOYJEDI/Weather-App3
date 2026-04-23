74db62e089d9f665717193dfbd1c670b230f33ca
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-04-23 17:32:32 +0200

    feat: implement getGeoData function to fetch geographical data based on user input

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./style.css"
+
+async function getGeoData() {
+    let search = txtSearch.value;
+
+    const url = `https://nominatim.openstreetmap.org/search?q=${search}&format=jsonv2&addressdetails=1`;
+}
+
+
+
+
+
+
