c9effd8ad6cb10cbf797282f340ddf08228107b9
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-04-23 17:37:04 +0200

    feat: enhance getGeoData function to handle fetch response and extract latitude and longitude

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ async function getGeoData() {
     let search = txtSearch.value;
 
     const url = `https://nominatim.openstreetmap.org/search?q=${search}&format=jsonv2&addressdetails=1`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        //console.log(result);
+
+        let lat = result[0].lat;
+        let lon = result[0].lon;
+    }
 }
 
 
