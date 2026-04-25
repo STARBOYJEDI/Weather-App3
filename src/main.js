ab47adb17bebaa6bad8780b9d81fe8c903f61ddc
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-04-25 23:21:59 +0200

    feat: add DOM elements for displaying weather data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import "./style.css"
 
-
+const dvCityCountry = document.querySelector("#dvCityCountry");
+const dvCurrDate = document.querySelector("#dvCurrDate");
+const dvCurrTemp = document.querySelector("#dvCurrTemp");
+const pFeelsLike = document.querySelector("#pFeelsLike");
+const pHumidity = document.querySelector("#pHumidity");
+const pWind = document.querySelector("#pWind");
+const pPrecipitation = document.querySelector("#pPrecipitation");
 
 let cityName, countryName, weatherData;
 
